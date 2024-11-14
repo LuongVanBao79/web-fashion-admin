a3f89996d91404708fe4212cc000fda5c4eb82e6
Luong Van Bao <luongvanbaoabc@gmail.com>
2024-11-14 13:17:51 +0700

    xong video 3

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -9,15 +9,18 @@ import { FaCartArrowDown } from "react-icons/fa6";
 import { MdOutlineMessage } from "react-icons/md";
 import { IoNotificationsSharp } from "react-icons/io5";
 import { IoSettingsSharp } from "react-icons/io5";
+import { IoLogOutOutline } from "react-icons/io5";
 
 
 const Sidebar = () => {
 
-    const [activeTab, setActiveTab] = useState(null);
+    const [activeTab, setActiveTab] = useState(0);
+    const [isToggleSubmenu, setisToggleSubmenu] = useState(false);
 
 
-    const isOpenSubmenu = () => {
-        
+    const isOpenSubmenu = (index) => {
+        setActiveTab(index);
+        setisToggleSubmenu(!isToggleSubmenu);
     }
 
     return (
@@ -25,7 +28,8 @@ const Sidebar = () => {
             <ul>
                 <li>
                     <Link to='/'>
-                        <Button className="w-100">
+                        <Button className={`w-100 ${activeTab ===0  ? 'active' : ''}`}
+                        onClick={()=>isOpenSubmenu(0)}>
                             <span className="icon">
                                 <LuLayoutDashboard />
                             </span>
@@ -37,7 +41,8 @@ const Sidebar = () => {
                     </Link>
                 </li>
                 <li>
-                    <Button className={`w-100 ${activeTab ===1 ? 'active' : ''}`} onClick={()=>isOpenSubmenu(1)}>
+                    <Button className={`w-100 ${activeTab ===1 && isToggleSubmenu === true  ? 'active' : ''}`}
+                        onClick={()=>isOpenSubmenu(1)}>
                             <span className="icon">
                             <RiProductHuntFill />
                             </span>
@@ -46,7 +51,7 @@ const Sidebar = () => {
                                 <FaAngleRight />
                             </span>
                     </Button>
-                    <div className={`submenuWrapper ${activeTab ===1 ? 'open': 'close'}`}>
+                    <div className={`submenuWrapper ${activeTab ===1 && isToggleSubmenu === true ? 'colapse': 'colapsed'}`}>
                         <ul className='submenu'>
                             <li><Link to="#">Product List</Link></li>
                             <li><Link to="#">Product View</Link></li>
@@ -57,7 +62,8 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <Link to='/'>
-                        <Button className="w-100">
+                        <Button className={`w-100 ${activeTab ===2  ? 'active' : ''}`}
+                        onClick={()=>isOpenSubmenu(2)}>
                             <span className="icon">
                             <FaCartArrowDown />
                             </span>
@@ -70,7 +76,8 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <Link to='/'>
-                        <Button className="w-100">
+                        <Button className={`w-100 ${activeTab ===3  ? 'active' : ''}`}
+                        onClick={()=>isOpenSubmenu(3)}>
                             <span className="icon">
                             <MdOutlineMessage />
                             </span>
@@ -83,7 +90,8 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <Link to='/'>
-                        <Button className="w-100">
+                        <Button className={`w-100 ${activeTab ===4  ? 'active' : ''}`}
+                        onClick={()=>isOpenSubmenu(4)}>
                             <span className="icon">
                             <IoNotificationsSharp />
                             </span>
@@ -96,7 +104,8 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <Link to='/'>
-                        <Button className="w-100">
+                        <Button className={`w-100 ${activeTab ===5 ? 'active' : ''}`}
+                        onClick={()=>isOpenSubmenu(5)}>
                             <span className="icon">
                             <IoSettingsSharp />
                             </span>
@@ -109,6 +118,13 @@ const Sidebar = () => {
                 </li>
                 
             </ul>
+
+            <br/>
+                <div className='logoutWrapper'>
+                    <div className='logoutBox'>
+                        <Button variant="contained"><IoLogOutOutline />Logout</Button>
+                    </div>
+                </div>
         </div>
     )
 }
